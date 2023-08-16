9c5ea632ff1acfe619f558e5feb8e497e1d3301e
typeSpindlerJ <okay>
2023-08-15 20:12:03 -0700

    The start of something great

---
+++ Pages/Controller1.razor.cs
@@ -0,0 +1,47 @@
+﻿using Kevsoft.WLED;
+using System.Xml;
+
+namespace LEDControl.Pages
+{
+    public partial class Controller1
+    {
+        private int currentCount = 0;
+        private WLedClient _client = new("http://192.168.0.100/");
+
+        private async void TurnOff()
+        {
+            currentCount++;
+
+            await _client.Post(new StateRequest { On = false });
+
+        }
+
+        private async void TurnOn()
+        {
+            currentCount++;
+            await _client.Post(new StateRequest { On = true });
+
+        }
+
+        private async void Effect()
+        {
+            currentCount++;
+
+
+            var something = new StateRequest()
+            {
+                On = true,
+                Brightness = 128,
+                MainSegment = new SegmentRequest()
+                {
+                    EffectId = 
+                }
+            };
+
+  
+
+
+            await _client.Post(new SegmentRequest[] {en});
+        }
+    }
+}
